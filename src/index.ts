1a2f8481400df63517a5b9efa8035983046ff7cf
Björn Büttner <Idrinth@users.noreply.github.com>
2024-04-18 23:54:08 +0200

    actually check out repository of other pull requests

--- src/index.ts
+++ src/index.ts
@@ -127,9 +127,7 @@ class Conflibot {
           await this.system(
             `git remote add ${target.head.repo.owner.login} ${target.head.repo.url}`,
           );
-          await this.system(
-            `git fetch ${target.head.repo.owner.login} --all`,
-          );
+          await this.system(`git fetch ${target.head.repo.owner.login} --all`);
         }
         await this.system(
           `git format-patch origin/${pull.data.base.ref}.. ${target.head.repo.owner.login}/${target.head.ref} --stdout | git apply --check`,
